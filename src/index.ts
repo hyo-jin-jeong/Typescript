bf4ff929f10c756d0416482583a352d08267e1a0
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-12-09 01:45:41 +0900

    Chore: pacakge.json start 방식 변경
    src, dist 폴더 생성
    .gitignore dist 추가
    tsconfig.json include 변경

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const sayHi = (name: String, age: Number, gender: String): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 }
 
 console.log(sayHi("Nicolas", 444, "male"));
